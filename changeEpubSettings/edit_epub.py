2a87c5ebf57b1faaada6edcad2ffb4d12a1e506c
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2023-02-25 12:06:07 +0300

    an upgrade to make the code work with all paths

--- changeEpubSettings/edit_epub.py
+++ changeEpubSettings/edit_epub.py
@@ -1,27 +1,26 @@
-import os
-import shutil
-import glob
+import os, shutil, glob
 from zipfile import ZipFile
 
 #Change Epub to zip
-epubDir = r'C:\Users\ahmet\Documents\GitHub\python-projects\changeEpubSettings\Coral of the Moon.epub'
+epubDir = input("Enter the path: ")
 epubName = list(epubDir.split('\\'))[-1]
-newName = epubName.replace(".epub" , ".zip")
 
-os.rename(epubName, newName)
+newName = epubDir.replace(".epub", ".zip")
+
+os.rename(epubDir, newName)
 
 #Extract The zip
-folderName = epubName.replace(".epub", "")
-zipPath = r'C:\Users\ahmet\Documents\GitHub\python-projects\changeEpubSettings' + "\\" + folderName
+folderName = epubName.replace(".epub", "") # folderName == Coral of the Moon, THe folder we want to extract
+zipPath = epubDir.replace(".epub", "")
 
-with ZipFile(r"C:\Users\ahmet\Documents\GitHub\python-projects\changeEpubSettings\Coral of the Moon.zip", 'r') as zObject:
+with ZipFile(epubDir.replace(".epub", ".zip"), 'r') as zObject:
     zObject.extractall(
         path = zipPath
     )
 
 # Find the styles
 files = glob.glob(
-    r'C:\Users\ahmet\Documents\GitHub\python-projects\changeEpubSettings\Coral of the Moon\**\*.css', 
+    pathname = zipPath + '\**\*.css', 
     recursive = True
 )
 
@@ -34,39 +33,12 @@ cssFileName = cssFile.split("\\")[-1]
 #change the former style with the new one
 os.remove(cssFile)
 
-shutil.copy(r'C:\Users\ahmet\Documents\GitHub\python-projects\changeEpubSettings\stylesheet.css',
-            r'C:\Users\ahmet\Documents\GitHub\python-projects\changeEpubSettings\Coral of the Moon\OEBPS\Styles' + "\\" + cssFileName)
+shutil.copy('stylesheet.css',
+            cssFile)
+n = shutil.make_archive(folderName, "zip", zipPath)
 
+os.rename(n, epubDir)
+os.remove(newName)
 
-src = r'C:\Users\ahmet\Documents\GitHub\python-projects\changeEpubSettings\Coral of the Moon'
-n = shutil.make_archive(folderName + "-Test", "zip", src)
+os.startfile(epubDir)
 
-os.rename(n, epubName)
-os.startfile(epubName)
-
-# def main():
-
-#     src = r'C:\Users\ahmet\Desktop\Masaüstü\Japon\LightNovels\Mushoku Tensei\mushoku'
-
-#     if not os.path.isfile("mushoku.epub"):
-
-#         fileName = shutil.make_archive("mushoku", "zip", src)
-#         base = os.path.splitext(fileName)[0]
-
-#         target = base + '.epub'
-#         os.rename(fileName, target)
-
-#     else: 
-#         os.remove("mushoku.epub")
-
-#         fileName = shutil.make_archive("mushoku", 'zip', src)
-#         base = os.path.splitext(fileName)[0]
-
-#         target = base + '.epub'
-
-#         os.rename(fileName, target)
-
-
-
-# if __name__ ==  "__main__":
-#     main()
